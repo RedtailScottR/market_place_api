58ac5a4f4e9b5d7d10f20c798f30833092a6df20
RedtailScottR <scott.ristow@redtailtechnology.com>
2017-08-17 18:15:31 -0700

    Set up routes, fixed warden error

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,8 @@ require 'api_constraints'
 MarketPlaceApi::Application.routes.draw do
   devise_for :users
   namespace :api, defaults: { format: :json }, constraints: { subdomain: 'api' }, path: '/'  do
-     scope module: :v1,
-              constraints: ApiConstraints.new(version: 1, default: true) do
+     scope module: :v1, constraints: ApiConstraints.new(version: 1, default: true) do
+      	resources :users, :only => [:show]
       # We are going to list our resources here
     end
   end
